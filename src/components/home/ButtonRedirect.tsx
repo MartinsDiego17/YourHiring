d0a2e11121c9dda721eac396bf66dc352956522c
Diego <martinsdiego0513@outlook.es>
2024-07-31 21:15:04 -0300

    add function button

---
+++ src/components/home/ButtonRedirect.tsx
@@ -0,0 +1,22 @@
+import { Button, Link } from "@nextui-org/react"
+
+const ButtonRedirect = () => {
+
+    const smoothScroll = () => {
+        document.querySelector("#hola")?.scrollIntoView({
+            behavior: 'smooth'
+        });
+    };
+
+    return (
+        <Link className="smooth-scroll mt-[1%]"
+            onClick={smoothScroll}>
+            <Button onClick={smoothScroll} className="2xl:text-[1.3rem] 2xl:py-[35px] 2xl:px-[40px] extraLight mt-[2%] buttonDefault rounded-full py-[25px] px-[30px] text-[1rem]" >
+                Mis postulaciones <i className="iconAwesome fa-solid fa-arrow-right-long"></i>
+            </Button>
+        </Link>
+
+    )
+}
+
+export default ButtonRedirect
